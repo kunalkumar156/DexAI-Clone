eae7af5ef8cd834112d69d461f332c4be1d6bd2b
Kishor <yoyokunal156@gmail.com>
2023-08-13 21:13:52 +0530

    buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,66 @@
+import { Box, Button, Container, Flex, Text } from "@chakra-ui/react";
+import { FiArrowUpRight } from "react-icons/fi";
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <>
+      <Container maxW={1300}>
+        <Box>
+          <Flex
+            justifyContent="center"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Text>Whatever You Want to ask - DEX.AI </Text>
+            <Text>
+              has the <span></span>
+              Solutions
+            </Text>
+            <Text>
+              Artificial intelligence makes it fast easy to create content for
+              your blog, social media, website, and more! Rated 5/5 stars in
+              3,000+ reviews.
+            </Text>
+          </Flex>
+        </Box>
+
+        <Flex justifyContent="center" gap={20}>
+          <button className="gradient-button">
+            start a free trial
+            <FiArrowUpRight
+              className="arrow"
+              style={{
+                background: "transparent",
+                fontSize: "20px",
+                transition: "transform 0.5s ease",
+              }}
+            />
+          </button>
+
+          <Button
+            className="button"
+            borderRadius={30}
+            color="#fff"
+            padding="24px 26px "
+            textAlign="center"
+            backgroundColor="transparent"
+            _hover={{ backgroundColor: "transparent" }}
+          >
+            how dex.ai work
+            <FiArrowUpRight
+              className="arrow"
+              style={{
+                background: "transparent",
+                fontSize: "20px",
+                transition: "transform 0.5s ease",
+              }}
+            />
+          </Button>
+        </Flex>
+      </Container>
+    </>
+  );
 }
 
 export default Hero;
