b8ad41c21f0a8cf915a5a475989abdb3fae4b95d
Kishor <yoyokunal156@gmail.com>
2023-08-13 15:28:40 +0530

    nav

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -2,12 +2,15 @@ import {
   Button,
   Container,
   Flex,
+  Icon,
   IconButton,
   Image,
   Link,
   Text,
+  color,
 } from "@chakra-ui/react";
 import { FiArrowUpRight } from "react-icons/fi";
+import { BiChevronDown } from "react-icons/bi";
 import React from "react";
 
 function Nav() {
@@ -30,31 +33,97 @@ function Nav() {
 
           {/* Links */}
 
-          <Flex py="4">
-            <Link href="/">
-              <Text>Home</Text>
+          <Flex>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Text
+                padding="30px 30px"
+                fontSize="16px"
+                fontWeight="600"
+                color="white"
+                transition="color 0.3s ease"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                _hover={{
+                  color: "#6720FF",
+                }}
+              >
+                Home
+                <BiChevronDown />
+              </Text>
             </Link>
 
-            <Link href="#">
-              <Text>About Us</Text>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Text
+                padding="30px 30px"
+                fontSize="16px"
+                fontWeight="600"
+                color="white"
+                transition="color 0.3s ease"
+                _hover={{ color: "#6721FF" }}
+              >
+                About Us
+              </Text>
             </Link>
 
-            <Link href="#">
-              <Text>Pages</Text>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Text
+                padding="30px 30px"
+                fontSize="16px"
+                fontWeight="600"
+                color="white"
+                transition="color 0.3s ease"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                _hover={{ color: "#6721FF" }}
+              >
+                Pages <BiChevronDown />
+              </Text>
             </Link>
 
-            <Link href="#">
-              <Text>News</Text>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Text
+                padding="30px 30px"
+                fontSize="16px"
+                fontWeight="600"
+                color="white"
+                transition="color 0.3s ease"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                _hover={{
+                  color: "#6720FF",
+                }}
+              >
+                News <BiChevronDown />
+              </Text>
             </Link>
 
-            <Link href="#">
-              <Text>Contacts</Text>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Text
+                padding="30px 30px"
+                fontSize="16px"
+                fontWeight="600"
+                color="white"
+                transition="color 0.3s ease"
+                _hover={{
+                  color: "#6720FF",
+                }}
+              >
+                Contacts
+              </Text>
             </Link>
           </Flex>
           <Flex justify="center" align="center" gap={10}>
             {/* language */}
-            <Link href="#">
-              <Text>English</Text>
+            <Link href="#" style={{ textDecoration: "none" }}>
+              <Flex alignItems="center">
+                {" "}
+                {/* Use Flexbox for layout */}
+                <Text marginRight="4px">EN</Text>
+                <BiChevronDown />
+              </Flex>
             </Link>
 
             {/* Sign Up Button */}
