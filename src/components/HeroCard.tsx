6cac6beaa281e011fe6a062072b978db75e379ba
Kishor <yoyokunal156@gmail.com>
2023-08-15 11:44:35 +0530

    editing hero

--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,7 +1,14 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
 
 function HeroCard() {
-  return <div>HeroCard</div>;
+  return (
+    <>
+      <Container maxW={1300}>
+        <Box></Box>
+      </Container>
+    </>
+  );
 }
 
 export default HeroCard;
