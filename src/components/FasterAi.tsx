9c3fa898b523664f7ebceac7c42a1b0ec9f79673
Kishor <yoyokunal156@gmail.com>
2023-08-19 18:50:01 +0530

    Card made

--- src/components/FasterAi.tsx
+++ src/components/FasterAi.tsx
@@ -5,16 +5,24 @@ import {
   Flex,
   Heading,
   Text,
-  background,
+  Input,
 } from "@chakra-ui/react";
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderMark,
+} from "@chakra-ui/react";
+import { RiArrowRightSLine } from "react-icons/ri";
 import React from "react";
 
 function FasterAi() {
   return (
     <>
       <Container maxW={1300}>
-        <Flex>
-          <Box width="40%">
+        <Flex gap={24}>
+          <Box width="35%">
             <Heading
               fontWeight="700"
               fontSize="60px"
@@ -37,7 +45,7 @@ function FasterAi() {
           </Box>
 
           <Box width="60%">
-            <Flex justifyContent="center" alignItems="center">
+            <Flex alignItems="center">
               <Box
                 background="linear-gradient(89.78deg, rgba(200 189 255 / 20%) -31.69%, rgba(186 166 255 / 20%) -22.78%, rgba(103 33 255 / 20%) 27.93%, rgba(0 203 255 / 20%) 99.79%)"
                 borderRadius="10px"
@@ -54,7 +62,6 @@ function FasterAi() {
                   fontSize="15px"
                   padding="6px 15px"
                   _hover={{ background: "#6721FF" }}
-                  onClick={{ background: "#6721ff" }}
                 >
                   Website
                 </Button>
@@ -112,6 +119,119 @@ function FasterAi() {
                 </Button>
               </Box>
             </Flex>
+
+            <Box
+              background="linear-gradient(89.78deg, rgba(200 189 255 / 20%) -31.69%, rgba(186 166 255 / 20%) -22.78%, rgba(103 33 255 / 20%) 27.93%, rgba(0 203 255 / 20%) 99.79%)"
+              borderRadius="30px"
+              border="1px solid rgba(255 255 255 / 20%)"
+              padding="30px 40px"
+              width="60%"
+            >
+              {/* First Section */}
+              <Box mb="4" background="transparent">
+                <Text
+                  bg="transparent"
+                  fontWeight="600"
+                  fontSize="16px"
+                  mb="0.5rem"
+                  marginTop="1.5rem"
+                >
+                  Keywords
+                </Text>
+                <Input
+                  fontSize="14px"
+                  bg="rgba(255, 255, 255, 0.12)"
+                  border="1px solid rgba(255, 255, 255, 0.12)"
+                  placeholder="AI 2023"
+                  mb="10px"
+                  _hover={{ border: "1px solid rgba(255, 255, 255, 0.12)" }}
+                  _focus={{
+                    outline: "none",
+                    boxShadow: "none",
+                  }}
+                />
+
+                <Text
+                  bg="transparent"
+                  fontWeight="600"
+                  fontSize="16px"
+                  mb="0.5rem"
+                >
+                  Creativity
+                </Text>
+                <Slider
+                  aria-label="slider-ex-4"
+                  defaultValue={30}
+                  bg="transparent"
+                >
+                  <SliderTrack bg="#6721ff">
+                    <SliderFilledTrack bg="#6721ff" />
+                  </SliderTrack>
+                  <SliderThumb boxSize={4} bg="#6721FF">
+                    <Box />
+                  </SliderThumb>
+                </Slider>
+              </Box>
+
+              {/* Second Section */}
+              <Flex alignItems="center" background="transparent" gap={7}>
+                <Box width="25%" bg="transparent">
+                  <Text
+                    bg="transparent"
+                    fontWeight="600"
+                    fontSize="16px"
+                    mb="0.5rem"
+                  >
+                    Language
+                  </Text>
+                  <Input
+                    bg="rgba(255, 255, 255, 0.12)"
+                    fontSize="14px"
+                    border="1px solid rgba(255, 255, 255, 0.12)"
+                    placeholder="Swedish"
+                    mb="10px"
+                    _hover={{ border: "1px solid rgba(255, 255, 255, 0.12)" }}
+                    _focus={{
+                      outline: "none",
+                      boxShadow: "none",
+                    }}
+                  />
+                </Box>
+                <Box background="transparent">
+                  <Text
+                    bg="transparent"
+                    fontWeight="600"
+                    fontSize="16px"
+                    mb="0.5rem"
+                  >
+                    Language
+                  </Text>
+                  <Input
+                    fontSize="14px"
+                    bg="rgba(255, 255, 255, 0.12)"
+                    border="1px solid rgba(255, 255, 255, 0.12)"
+                    placeholder="Professional"
+                    mb="10px"
+                    _hover={{ border: "1px solid rgba(255, 255, 255, 0.12)" }}
+                    _focus={{
+                      outline: "none",
+                      boxShadow: "none",
+                    }}
+                  />
+                </Box>
+              </Flex>
+
+              {/* Button */}
+              <button className="gradient-button btn-1 card-button">
+                Generate
+                <RiArrowRightSLine
+                  style={{
+                    background: "transparent",
+                    fontSize: "20px",
+                  }}
+                />
+              </button>
+            </Box>
           </Box>
         </Flex>
       </Container>
