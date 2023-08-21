785a16130f1e56570516ea3e5f6dfbc72060fa38
Kishor <yoyokunal156@gmail.com>
2023-08-21 19:41:41 +0530

    Howitworks section started

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -4,7 +4,17 @@ import React from "react";
 function HowItWorks() {
   return (
     <>
-      
+      <Container maxW={1300} pt="10rem">
+        <Heading
+          fontWeight="700"
+          fontSize="60px"
+          letterSpacing="-0.01em"
+          textAlign="center"
+          mb="5rem"
+        >
+          How it <span>works</span>
+        </Heading>
+      </Container>
     </>
   );
 }
