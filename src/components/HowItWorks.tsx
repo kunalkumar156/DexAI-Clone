f764dc758243497096641f8d873ddbc924bb74f9
Kishor <yoyokunal156@gmail.com>
2023-08-25 00:15:40 +0530

    added money

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -4,7 +4,19 @@ import React from "react";
 function HowItWorks() {
   return (
     <>
-      <Container maxW={1300} pt="10rem"></Container>
+      <Container maxW={1300} pt="10rem" pb="10rem">
+        <Heading
+          fontWeight="700"
+          fontSize="60px"
+          letterSpacing="-0.01em"
+          mb="2rem"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          AI Does it faster & better.
+        </Heading>
+      </Container>
     </>
   );
 }
--- src/components/Money.tsx
+++ src/components/Money.tsx
@@ -1,7 +1,38 @@
+import { Box, Container, Heading, Stack, Switch, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Money() {
-  return <div>Money</div>;
+  return (
+    <>
+      <Container maxW={1300}>
+        {" "}
+        <Heading
+          fontWeight="700"
+          fontSize="60px"
+          letterSpacing="-0.01em"
+          mb="2rem"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          Money well invested
+        </Heading>
+        <Box
+          className="checkbox"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          gap={5}
+        >
+          <Text>Yearly</Text>
+          <Stack direction="row" color="#6721FF">
+            <Switch colorScheme="purple" size="lg" />
+          </Stack>
+          <Text>Monthly</Text>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default Money;
