e52efb60027aecf273482165a7cf3a645960b3d8
Kishor <yoyokunal156@gmail.com>
2023-08-31 23:54:11 +0530

    random

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -2,23 +2,7 @@ import { Container, Box, Heading } from "@chakra-ui/react";
 import React from "react";
 
 function HowItWorks() {
-  return (
-    <>
-      <Container maxW={1300} pt="10rem" pb="10rem">
-        <Heading
-          fontWeight="700"
-          fontSize="60px"
-          letterSpacing="-0.01em"
-          mb="2rem"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          AI Does it faster & better.
-        </Heading>
-      </Container>
-    </>
-  );
+  return <></>;
 }
 
 export default HowItWorks;
