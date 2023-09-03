159261d7787c64a2b7cdbc2eb1e48e640ae4dccc
Kishor <yoyokunal156@gmail.com>
2023-09-03 23:00:30 +0530

    dummy cardss made

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,24 +1,88 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
-import React from "react";
+// components/CardContainer.js
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 
-function HowItWorks() {
+const Card = ({ title, options }) => (
+  <Box
+    border="1px solid white"
+    p={4}
+    borderRadius="md"
+    bg="transparent"
+    color="white"
+    maxW="300px"
+    m={4}
+  >
+    <Heading as="h2" size="md" mb={2}>
+      {title}
+    </Heading>
+    <Box>
+      {options.map((option, index) => (
+        <Text key={index} mb={1} overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis">
+          {option}
+        </Text>
+      ))}
+    </Box>
+    <Button colorScheme="teal" mt={4}>
+      Button
+    </Button>
+  </Box>
+);
+
+const CardContainer = () => {
+  const cards = [
+    {
+      title: "Card 1",
+      options: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10",
+      ],
+    },
+    {
+      title: "Card 2",
+      options: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10",
+      ],
+    },
+    {
+      title: "Card 3",
+      options: [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 7",
+        "Option 8",
+        "Option 9",
+        "Option 10",
+      ],
+    },
+  ];
+
   return (
-    <>
-      <Container maxW={1300} pt="10rem" pb="10rem">
-        <Heading
-          fontWeight="700"
-          fontSize="60px"
-          letterSpacing="-0.01em"
-          mb="2rem"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          AI Does it faster & better.
-        </Heading>
-      </Container>
-    </>
+    <Box maxW="1300px" mx="auto" p={4} display="flex" flexWrap="wrap">
+      {cards.map((card, index) => (
+        <Card key={index} {...card} />
+      ))}
+    </Box>
   );
-}
+};
 
-export default HowItWorks;
+export default CardContainer;
