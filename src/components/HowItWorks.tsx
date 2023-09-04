242ee8750d6ccf3fa7bbc3b8d3b70e798de47341
Kishor <yoyokunal156@gmail.com>
2023-09-04 23:45:42 +0530

    new

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -11,6 +11,9 @@ const Card = ({ title, options }: { title: "srting"; options: "string" }) => (
     maxW="300px"
     m={4}
   >
+    <Heading as="h2" size="md" mb={2}>
+      {title}
+    </Heading>
     <Box>
       {options.map((option, index) => (
         <Text
